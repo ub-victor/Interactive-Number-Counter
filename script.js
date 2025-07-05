a86f866f12f303d8d9662e2721d44c18daef2cfc
ub-victor <victoireushindi371@gmail.com>
2025-07-05 12:07:05 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function()){
+    // Get DOM elememts
+    const counterElement = document.getElementById('counter');
+    const increaseBnt = document.getElementById('increase');
+    const decreaseBtn = document.getElementById('decrease');
+    const resetBtn = document.getElementById('reset');
+
+    // 
+}
