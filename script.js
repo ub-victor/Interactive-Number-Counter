9762f98422e03560675bc5577c2def8613afa0cd
ub-victor <victoireushindi371@gmail.com>
2025-07-05 13:06:59 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function()){
+document.addEventListener('DOMContentLoaded', function(){
     // Get DOM elememts
     const counterElement = document.getElementById('counter');
     const increaseBnt = document.getElementById('increase');
     const decreaseBtn = document.getElementById('decrease');
     const resetBtn = document.getElementById('reset');
 
-    // Display current year
+    // Set current year in footer
     document.getElementById('current-year').textContent = new Date().getFullYear();
-}
+});
