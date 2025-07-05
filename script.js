6a7e73ba56e1d0a6cfdf8aee5e2525038dfcf155
ub-victor <victoireushindi371@gmail.com>
2025-07-05 13:38:51 +0200

    Initial commit

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // Set current year in footer
     document.getElementById('current-year').textContent = new Date().getFullYear();
+
+    // Initalize counter
+    let counter = 0;
+
+    // Update counter display
+    function UpdateCounter(){
+        counterElement.textContent = counter
+    }
+
+    // Change color based on value
+
+    if(counter > 0){
+        counterElement.style.color = "var(--positive-color)";
+    }else if (counter < 0){
+        counterElement.style.color = "var(--negative-color)";
+    }else{
+        counterElement.style.color = "var(--zero-color)"
+    }
 });
